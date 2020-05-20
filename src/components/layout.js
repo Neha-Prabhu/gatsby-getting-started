23f0c4588c4bd4a0c5c14d8b8c7382db49264955
Guru Sainath <gurusainath007@gmail.com>
2020-05-21 01:12:39 +0530

    Bootstrap and react bootstrap issue resolved

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,48 @@
 import React from "react"
 import { Link } from "gatsby"
-import {Navbar,Nav,Container} from "react-bootstrap"
+import { Navbar, Nav, Container } from "react-bootstrap"
 
-const Header=  ({siteTitle})=> {
-    return(
+const Header = ({ siteTitle }) => {
+  return (
     <div>
-    <header>
+      <header>
         <Container>
-            <Navbar expand="md">
-                <Navbar.Brand href="/"> {siteTitle} </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-nav-bar"/>
-                <Navbar.Collapse id="basic-nav-nav">
-                    <Nav as="ul" className = "ml-auto">
-                        <Nav.Item as="li">
-                            <Link to="/about/" className="nav-link" activeClassName="active">About</Link>
-                        </Nav.Item>
-                        <Nav.Item as="li">
-                            <Link to="/contact/" className="nav-link" activeClassName="active">Contact</Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+          <Navbar expand="md">
+            <Navbar.Brand href="/"> {siteTitle} </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-nav-bar" />
+            <Navbar.Collapse id="basic-nav-nav">
+              <Nav as="ul" className="ml-auto">
+                <Nav.Item as="li">
+                  <Link
+                    to="/about/"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    About
+                  </Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                  <Link
+                    to="/contact/"
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    Contact
+                  </Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </Container>
-    </header>
+      </header>
     </div>
-    );
+  )
 }
 
 export default function Layout({ children }) {
   return (
-      <div>
-      <Header siteTitle="wisebit.dev"/>
+    <div>
+      <Header siteTitle="wisebit.dev" />
       {children}
     </div>
   )
